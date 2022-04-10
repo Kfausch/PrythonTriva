176e37123b335a4fd7d8613c5c46f4bb996c7b21
Kevin Fausch <54674691+Kfausch@users.noreply.github.com>
2022-04-10 11:45:45 -0500

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,60 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+THEME_COLOR = "#375362"
+FONT = ("Arial", 15, "italic")
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Trivia")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.canvas = Canvas(height=250, width=300, highlightthickness=0, bg="white")
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        self.score_label = Label(text=f"Score: {self.score}", font=("Arial", 13, "bold"), bg=THEME_COLOR, fg="white")
+        self.score_label.grid(column=1, row=0)
+
+        self.question_text = self.canvas.create_text(150,125,
+                                                     text="Question goes here",
+                                                     fill="black",
+                                                     font=FONT,
+                                                     width=280
+                                                     )
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, command=self.check_false)
+        self.false_button.grid(column=0, row=2)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, command=self.check_true, )
+        self.true_button.grid(column=1, row=2)
+        self.get_next_question()
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def check_true(self):
+        self.give_feedback(self.quiz.check_answer("true"))
+
+    def check_false(self):
+        self.give_feedback(self.quiz.check_answer("false"))
+
+    def give_feedback(self, is_right):
+        if is_right == True:
+            self.canvas.config(bg ="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+    def score(self):
+        self.score_label
